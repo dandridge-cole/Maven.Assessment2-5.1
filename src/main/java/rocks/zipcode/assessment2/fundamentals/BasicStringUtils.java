c1dec82a17465d2eca31cc026d00d53bd25c7009
Dandridge Cole <dandridgecole@yahoo.com>
2019-06-17 10:32:42 -0400

    basic string utils all tests passing

--- src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
+++ src/main/java/rocks/zipcode/assessment2/fundamentals/BasicStringUtils.java
@@ -7,7 +7,7 @@ public class BasicStringUtils {
      * @return concatenation of `string1` and `string2`
      */
     public static String concatentate(String string1, String string2) {
-        return null;
+        return string1+string2;
     }
 
     /**
@@ -15,7 +15,8 @@ public class BasicStringUtils {
      * @return an identical string with characters in reverse order
      */
     public static String reverse(String string1) {
-        return null;
+        StringBuilder sb = new StringBuilder(string1);
+        return sb.reverse().toString();
     }
 
     /**
@@ -24,7 +25,7 @@ public class BasicStringUtils {
      * @return concatenation of the reverse of `string1` and reverse of `string2`
      */
     public static String reverseThenConcatenate(String string1, String string2) {
-        return null;
+        return concatentate(reverse(string1),reverse(string2));
     }
 
     /**
@@ -33,7 +34,9 @@ public class BasicStringUtils {
      * @return `string` with `charactersToRemove` removed
      */
     public static String removeCharacters(String string, String charactersToRemove) {
-        return null;
+        for(Character c: charactersToRemove.toCharArray())
+        string=string.replace(c.toString(),"");
+        return string;
     }
 
     /**
@@ -42,6 +45,6 @@ public class BasicStringUtils {
      * @return reverse of `string` with `charactersToRemove` removed
      */
     public static String removeCharactersThenReverse(String string, String charactersToRemove) {
-        return null;
+        return reverse(removeCharacters(string,charactersToRemove));
     }
 }
